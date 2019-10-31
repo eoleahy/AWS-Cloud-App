fcbdc45f015fe5fae4eb2a163008eddf31022a51
eoleahy <eoin.97@live.ie>
2019-10-31 16:44:23 +0000

    buttons kinda work

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const path = require("path");
 
 
 AWS.config.loadFromPath('./config.json');
-AWS.config.update({
-    endpoint: "http://localhost:8000"
-});
+//AWS.config.update({
+//    endpoint: "http://localhost:8000"
+//});
 
 let s3 = new AWS.S3({
     apiVersion: "2006-03-01"
@@ -23,12 +23,14 @@ let publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
 app.get('/create', getBucket);
+app.get('/create2', createDatabase);
+app.get('/delete', deleteDatabase);
 app.get('/', (req, res) => res.sendFile(publicPath + "/client.html"));
 
 
 app.listen(port, () => console.log(`To view webpage visit ${port}`));
 
-async function createDatabase() {
+function createDatabase(req, res) {
 
     var dynamodb = new AWS.DynamoDB();
 
@@ -58,12 +60,32 @@ async function createDatabase() {
         }
     };
 
-
     dynamodb.createTable(params, (err, data) => {
-        if (err) console.error("Unable to create table.");
+        if (err) console.log("Unable to create table.");
 
         else
             console.log("Table created!");
+
+        res.send(data);
+    });
+
+}
+
+async function deleteDatabase(req, res) {
+
+    let dynamodb = new AWS.DynamoDB();
+
+    let params = {
+        TableName: "Movies"
+    };
+
+    dynamodb.deleteTable(params, (err, data) => {
+        if (err) console.log("unable to delete");
+
+        else
+            console.log("Table deleted");
+
+        res.send(data); 
     });
 }
 
@@ -81,6 +103,7 @@ async function getBucket() {
         if (err) console.error(err);
         console.log(data.Body.toString());
     });
+    
 }
 
 
@@ -100,4 +123,5 @@ function sendWeather(req, res) {
         .catch(err => {
             res.send("Error 400: Bad request");
         });
+    
 }
